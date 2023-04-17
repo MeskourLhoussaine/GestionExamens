19e95ac2d68e8fc5aa11369211a50375d2089db4
mabroukali025 <118557039+mabroukali025@users.noreply.github.com>
2023-04-17 23:45:58 +0000

    classe prof ,etudiant

--- src/main/java/ma/projet/beans/entities/Etudiant.java
+++ src/main/java/ma/projet/beans/entities/Etudiant.java
@@ -1,8 +1,40 @@
 package ma.projet.beans.entities;
 
-public class Etudiant {
+import java.util.Date;
+import java.util.List;
 
-	private String nom;
-	private String prenom;
-	private int id;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Entity
+@Table(name = "etudiants")
+@NoArgsConstructor
+@Getter(value = AccessLevel.PUBLIC)
+@Setter(value = AccessLevel.PUBLIC)
+public class Etudiant extends User{
+	@Column(nullable = false, length = 25,unique = true)
+	private int Num_Apo;
+	@Column(nullable = false, length = 25,unique = true)
+	private String Cne;
+	@Column(nullable = false, length = 25)
+	private String filier;
+	@Column(nullable = false, length = 25)
+	private String semester;
+	@Temporal(TemporalType.DATE)
+	@Column(nullable = false)
+	private Date date;
+	
+	
+	
 }
--- src/main/java/ma/projet/beans/entities/Professeur.java
+++ src/main/java/ma/projet/beans/entities/Professeur.java
@@ -1,5 +1,20 @@
 package ma.projet.beans.entities;
 
-public class Professeur {
+import java.util.Date;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+@Entity
+@Table(name = "professeurs")
+@NoArgsConstructor
+@Getter(value = AccessLevel.PUBLIC)
+@Setter(value = AccessLevel.PUBLIC)
+public class Professeur extends User{
+	@Column(nullable = false, length = 25)
+	private String specialite;
 }
