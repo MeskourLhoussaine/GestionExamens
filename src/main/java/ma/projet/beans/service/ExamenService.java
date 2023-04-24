8ccfd2d419df792ffd414b28a7fe825b8e93b77a
MeskourLhoussaine <127257257+MeskourLhoussaine@users.noreply.github.com>
2023-04-24 23:28:17 +0100

    service

--- src/main/java/ma/projet/beans/service/ExamenService.java
+++ src/main/java/ma/projet/beans/service/ExamenService.java
@@ -1,7 +1,38 @@
 package ma.projet.beans.service;
 
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import ma.projet.beans.entities.Examen;
 import ma.projet.beans.iservice.IExamen;
+import ma.projet.beans.repository.EtudiantRepository;
+import ma.projet.beans.repository.ExamenRepository;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+
+@Service
+@Transactional
+@AllArgsConstructor
 public class ExamenService implements IExamen {
+private ExamenRepository examenRepository;
+    @Override
+    public Examen save(Examen object) {
+        return this.examenRepository.save(object);
+    }
 
+    @Override
+    public void delete(Examen object) {
+   this.examenRepository.delete(object);
+    }
+
+    @Override
+    public Examen findById(int id) {
+        return this.examenRepository.findById(id);
+    }
+
+    @Override
+    public List<Examen> findAll() {
+        return this.examenRepository.findAll();
+    }
 }
